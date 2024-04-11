ca17ccac74afefba7558fa391160ae953f0ae33a
Pedro-2201 <pedrohmxavier@hotmail.com>
2024-04-11 19:13:44 -0300

    feat: first commit

---
+++ src/main/java/br/ufrn/imd/Gerente.java
@@ -0,0 +1,20 @@
+package br.ufrn.imd;
+
+public class Gerente extends Funcionario{
+    Nivel nivel;
+    public Gerente(int chMensal, double hh, Nivel nivel){
+        super(chMensal, hh);
+        this.nivel = nivel;
+    }
+    public double calcularSalario(){
+        return chMensal * hh + nivel.bonus;
+    }
+    public enum Nivel{
+        JUNIOR(1000), PLENO(2000), SENIOR(3000);
+        double bonus;
+
+         private Nivel(double bonus){
+            this.bonus = bonus;
+        }
+    }
+}
